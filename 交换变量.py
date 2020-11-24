cc8f4965b0ee7aa21fb0c91b475f126a291fb495
nagaresst <nagaresst@live.com>
2020-11-24 20:57:40 +0800

    test pychram

--- 交换变量.py
+++
@@ -1,8 +0,0 @@
-a = 13
-b = 20
-a = a ^ b
-print(a)
-b = a ^ b
-print(b)
-a = a ^ b
-print(a)
--- 字符编码集.py
+++
@@ -1,8 +0,0 @@
-# i = 65
-# for i in range(i, 127):
-#     print(chr(i))
-#     i += 1
-# print('hello'.encode('gbk'))
-
-y = '你好'.encode('utf8')
-print(y)
