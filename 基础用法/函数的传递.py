1cbc96fb541b274e7652e33fd8934540e1fdaff0
NagaResst <nagaresst@live.com>
2020-11-28 23:36:27 +0800

    update

--- 基础用法/函数的传递.py
+++ 基础用法/函数的传递.py
@@ -8,7 +8,6 @@ students = [
     {'name': 'Alex', 'age': '18', 'score': '98', }
 ]
 
-
 # # foo()函数返回了字典中的score元素的属性
 # def foo(ele):
 #     return ele['score']
@@ -21,3 +20,12 @@ students = [
 students.sort(reverse=True, key=lambda x: x['score'])
 print(students)
 
+from functools import reduce
+
+print(reduce(lambda x, y: x + y, [1, 2, 3, 4, 5]))
+# reduce函数可以按照自己定义的函数应用在可迭代对象内的第一个和第二个值上，最终将一个可迭代对象缩减成单个的值
+# dir可以列举出一个对象可以使用的方法
+print(dir(students))
+exit(996)
+
+# 以指定的代码退出程序
