e1e11c9eeb077a61d414bb60b32248e43963926b
nagaresst <nagaresst@live.com>
2020-12-15 18:58:25 +0800

    update

--- 生产脚本/request_test.py
+++ 生产脚本/request_test.py
@@ -1,37 +1,57 @@
 import requests
 import time
+import datetime
 
-site_url = ['', '', '', '', '', '']
+error_count = {}
+url_path = "url.txt"
+now_date = str(datetime.date.today())
+log = "/log/" + now_date
+count = 0
 
 
-def get_status_code(url):
-    page = requests.get(url)
+def get_url(path):
+    with open(path, 'r') as urls:
+        site_url_str = urls.read()
+        site_url = site_url_str.split(sep='\n')
+    urls.close()
+    return site_url
+
+
+def add_error_count(urls):
+    global error_count
+    global count
+    count += 1
+    error_count[urls] = count
+    return error_count
+
+
+def get_status_code(urls):
+    page = requests.get(urls)
     return page.status_code
 
 
-def log(url, status_code, state):
-    pass
-
-
-def created_ticket(url,code):
+def created_ticket(urls, page_code):
     header = {}
-    post_date = ''
-    response = requests.post()
+    post_date = '' + urls + '' + page_code + ''
+    response = requests.post('https://msp.deliverycenter.cn:8088', header, post_date)
     return response
 
 
-def test(url):
-    code = get_status_code(url)
-    if code == 200:
-        log(url, code, 'success')
-    # elif code == 300 or code == 301 or code == 302:
-    #     pass
-    else:
-        log(url, code, 'failed')
-        created_ticket(url, code)
-
-
+site_list = get_url(url_path)
 while True:
-    for url in site_url:
-        test(url)
+    with open(log + '/success.log', 'a') as success:
+        with open(log + 'failed.log', 'a') as failed:
+            for url in site_list:
+                code = str(get_status_code(url))
+                if code == '200':
+                    success.write(str(datetime.datetime.now()) + '|success[' + code + ']|' + url)
+                # elif code == '300' or code == '301' or code == '302':
+                #     pass
+                else:
+                    failed.write(str(datetime.datetime.now()) + '|failed[' + code + ']|' + url)
+                    add_error_count(url)
+                    if error_count[url] > 3:
+                        created_ticket(url, code)
+        failed.close()
+    success.close()
     time.sleep(180)
