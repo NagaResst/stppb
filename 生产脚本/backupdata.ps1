b6b09460d8d84e455159e83a486edd10c27ba6a0
nagaresst <nagaresst@live.com>
2022-02-08 11:24:48 +0800

    minio文件夹只对data文件夹进行打包压缩

--- 生产脚本/backupdata.ps1
+++ 生产脚本/backupdata.ps1
@@ -1,9 +1,9 @@
 #! /bin/pwsh
 $now=get-date -Format "yyyy-MM-dd"
 # 创建新的文件夹路径
-New-Item -Path "d:\backupdata\$now" -Type Directory
+New-Item -Path "d:\backupdata\" -Name $now -ItemType Directory
 set-location "d:\backupdata\$now"
 # 进入文件夹路径备份数据库 ， Windows环境需要提前给mysql设置环境变量
 mysqldump -uroot -pa0dy2vQI --all-databases > mysql.sql
 # 打包备份minio的数据
-Compress-Archive "d:\minio\" miniodata.zip
+Compress-Archive "d:\minio\data" miniodata.zip
