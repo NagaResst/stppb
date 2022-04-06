e838082bd735aea23a9ebfcc3378988d33eeb130
nagaresst <nagaresst@live.com>
2022-04-06 14:18:43 +0800

    update

--- 练习作业/testyaml.py
+++ 练习作业/testyaml.py
@@ -1,7 +1,9 @@
 import yaml
 
 path = r'yamlTest.yaml'
-yaml_file = (open(path, 'r')).read()
+yaml_file = (open(path, 'r', encoding='UTF-8')).read()
 print(yaml_file)
 yaml_str = yaml.load(yaml_file, yaml.FullLoader)
 print(yaml_str)
+to_str = yaml.dump(yaml_str)
+print(to_str)
