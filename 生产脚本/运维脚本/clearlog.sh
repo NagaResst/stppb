2e7d91c5e392b06e8a12200940d29d074dee85db
nagaresst <nagaresst@live.com>
2022-05-06 10:46:15 +0800

    update

--- 生产脚本/运维脚本/clearlog.sh
+++ 生产脚本/运维脚本/clearlog.sh
@@ -1,10 +1,7 @@
 tee /etc/cron.daily/clearlog.sh <<-'EOF'
 #! /bin/bash
-path=()
-for i in ${path[*]}
-do
-  find $i -type f -name "*.log" -mtime +30 -exec echo > {} \;
-done
+find /usr/local/webserver/tomcat-9.0.54/logs -type f -name "*.log" -mtime +180 -exec rm -f {} \;
+find /usr/local/webserver/provisioning/logs/server1 -type f -name "*.log" -mtime +180 -exec rm -f {} \;
 EOF
 chmod +x /etc/cron.daily/clearlog.sh
 systemctl restart crond
