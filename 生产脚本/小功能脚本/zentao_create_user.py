7c3c24a950a3867e934aec22e60769baae6d22c2
NagaResst <nagaresst@live.com>
2023-03-07 15:56:34 +0800

    update

--- 生产脚本/小功能脚本/zentao_create_user.py
+++ 生产脚本/小功能脚本/zentao_create_user.py
@@ -13,29 +13,14 @@ header = { "Token": token}
 # 添加用户
 url = path + "/users"
 date_today = datetime.datetime.now().strftime("%Y-%m-%d")
-data = {"account": "username", "password": "passwd", "realname": "",
-"dept": 10, "role": "dev","gender":"m", "join": date_today}
-result = requests.post(url=url, headers=header, json=data)
-print(result.text)
+user_list = [
+    {"account": "username", "password": "passwd", "realname": "","dept": 10, "role": "dev","gender":"m", "join": date_today},
+    {"account": "username", "password": "passwd", "realname": "","dept": 10, "role": "dev","gender":"m", "join": date_today},
+    {"account": "username", "password": "passwd", "realname": "","dept": 10, "role": "dev","gender":"m", "join": date_today},
+    {"account": "username", "password": "passwd", "realname": "","dept": 10, "role": "dev","gender":"m", "join": date_today},
+    {"account": "username", "password": "passwd", "realname": "","dept": 10, "role": "dev","gender":"m", "join": date_today}
+]
 
-
-# 脚本用完了需要进系统给用户添加权限
-
-
-#  部门编号
-# 1	咨询事业部
-# 2	项目事业部
-# 3	研发部
-# 4	智慧物资部
-# 5	大数据部
-# 6	孵化部
-# 7	业务一部
-# 8	业务二部
-# 9	业务三部
-# 10	外聘研发
-# 11	离职员工
-# 12	共享服务
-# 13	综合部
-# 14	客户
-# 15	共享公司
-
+for i in user_list:
+    result = requests.post(url=url, headers=header, json=i)
+    print(result.text)
